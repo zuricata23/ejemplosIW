85411cd59cbc495c2777638c7071f37a15b2c1db
cdelacruz <cdelacruz@ipn.mx>
2023-04-05 11:54:14 -0600

    Se modifica el ejemplo

--- JSON/gestionJSON.js
+++ JSON/gestionJSON.js
@@ -1,102 +1,80 @@
-/*
-   JSON: JavaScript Object Notation
 
-   1. Formato de datos, similar a un archivo de texto
-   2. Sintaxis parecida a objetos literales Javascript
-   3. Objeto JSON vs Objeto Javascript
-    
-   {
-     "boleta":"2018064025",
-     "nombre":"Juan Manuel Hernández"
-     "semestre":5
-     "promedio":8
-   }
+const opJson = document.querySelector("#opJson");
+const opAjax = document.querySelector("#opAjax");
+const opFetch = document.querySelector("#opFetch");
+const opEjemplo = document.querySelector("#opEjemplo");
 
-   La principal diferencia entre un objeto JSON y un objeto JavaScript 
-   normal -también llamado objeto literal - se reduce a las comillas. 
-   Todas las claves y valores de tipo cadena de un objeto JSON tienen que 
-   ir entre comillas dobles (").
+const jsonContainer = document.querySelector("#json");
+const ajaxContainer = document.querySelector("#ajax");
+const fetchContainer = document.querySelector("#fetch");
+const ejemploContainer = document.querySelector("#ejemplo");
 
-   Es decir, en formato Javascript el ejemplo de objeto queda como:
+let lectura = false;
 
-   const alumno = {
-     boleta:'2018064025',
-     nombre:'Juan Manuel Hernández'
-     semestre:5
-     promedio:8
-   }
+opJson.addEventListener("click", function (evento) {
+  jsonContainer.style.display="block";
+  ajaxContainer.style.display="none";
+  fetchContainer.style.display="none";
+  ejemploContainer.style.display="none";
+});
 
-   4. Arreglos JSON vs arreglos JavaScript
-   
-   Los arreglos JSON funcionan prácticamente igual que los arreglos en 
-   JavaScript, y pueden contener cadenas, booleanos, números y otros 
-   objetos JSON.
+opAjax.addEventListener("click", function (evento) {
+  jsonContainer.style.display="none";
+  ajaxContainer.style.display="block";
+  fetchContainer.style.display="none";
+  ejemploContainer.style.display="none";
+});
 
-   Ejemplo 1:
-   
-   [
-     {
-        "boleta":"2018064025",
-        "nombre":"Juan Manuel Hernández"
-        "semestre":5
-        "promedio":8
-     },
-     {
-        "boleta":"2019064225",
-        "nombre":"Oscar Miguel Martínez"
-        "semestre":4
-        "promedio":7.5
-     }
-   ]
+opFetch.addEventListener("click", function (evento) {
+  jsonContainer.style.display="none";
+  ajaxContainer.style.display="none";
+  fetchContainer.style.display="block";
+  ejemploContainer.style.display="none";
+});
 
-   Ejemplo 2:
+  opEjemplo.addEventListener("click", function (evento) {
+    jsonContainer.style.display="none";
+    ajaxContainer.style.display="none";
+    fetchContainer.style.display="none";
+    ejemploContainer.style.display="block";
+     
+    /* Código del ejemplo */
+    if (!lectura) {
+        fetch('alumnos.json')
+          .then(res => res.json()) // el método .json() analiza la respuesta JSON en un objeto literal JS
+          .then(data => {
+              let bloque = document.querySelector('#datosAlumnos');
+              let fila, colBoleta, colNombre, colSemestre, colPromedio, contBoleta, contNombre, contSemestre, contPromedio;
+      
+              bloque.style.display = "block";
+      
+              data.forEach(alumno => {
+                  fila = document.createElement("tr");
+                  colBoleta = document.createElement("td");
+                  colNombre = document.createElement("td");
+                  colSemestre = document.createElement("td");
+                  colPromedio = document.createElement("td");
 
-     {
-        "boleta":"2018064025",
-        "nombre":"Juan Manuel Hernández"
-        "semestre":5
-        "materias":[{"claveMat":"T101", "nombre":"Álgebra", "calif":8},
-                    {"claveMat":"T201", "nombre":"Programación", "calif":9},
-                    {"claveMat":"T202", "nombre":"Ética", "calif":8}]
-     }
+                  contBoleta = document.createTextNode(`${alumno.boleta}`);
+                  contNombre = document.createTextNode(`${alumno.nombre}`);
+                  contSemestre = document.createTextNode(`${alumno.semestre}`);
+                  contPromedio = document.createTextNode(`${alumno.promedio}`);
+                  colBoleta.appendChild(contBoleta);
+                  colNombre.appendChild(contNombre);
+                  colSemestre.appendChild(contSemestre);
+                  colPromedio.appendChild(contPromedio);
+          
+                  fila.appendChild(colBoleta);
+                  fila.appendChild(colNombre);
+                  fila.appendChild(colSemestre);
+                  fila.appendChild(colPromedio);
 
-*/
-
-fetch('alumnos.json')
-  .then(res => res.json()) // el método .json() analiza la respuesta JSON en un objeto literal JS
-  .then(data => {
-    let bloque = document.querySelector('#datosAlumnos');
-    let fila, colBoleta, colNombre, colSemestre, colPromedio, contBoleta, contNombre, contSemestre, contPromedio;
-    
-    bloque.style.display = "block";
-    
-
-    data.forEach(alumno => {
-        fila = document.createElement("tr");
-        colBoleta = document.createElement("td");
-        colNombre = document.createElement("td");
-        colSemestre = document.createElement("td");
-        colPromedio = document.createElement("td");
-
-        contBoleta = document.createTextNode(`${alumno.boleta}`);
-        contNombre = document.createTextNode(`${alumno.nombre}`);
-        contSemestre = document.createTextNode(`${alumno.semestre}`);
-        contPromedio = document.createTextNode(`${alumno.promedio}`);
-        colBoleta.appendChild(contBoleta);
-        colNombre.appendChild(contNombre);
-        colSemestre.appendChild(contSemestre);
-        colPromedio.appendChild(contPromedio);
-        
-        fila.appendChild(colBoleta);
-        fila.appendChild(colNombre);
-        fila.appendChild(colSemestre);
-        fila.appendChild(colPromedio);
-
-        bloque.appendChild(fila);
-        
-    });
-    
+                  bloque.appendChild(fila);
+              });
+        });
+        lectura = true;
+    }  
+  });
 
 
-  })
-  
+
