d830aca909c7045bf43e80552732f95e678a30e2
CDLCS <cdelacruz@ipn.mx>
2023-04-19 12:57:51 -0600

    se añade folder de práctica 1 JS

---
+++ Practica1JS/cursos.js
@@ -0,0 +1,30 @@
+/**
+ * Listado de cursos ofertados
+ */
+
+let cursos = [
+    {
+     nombre: "HTML",
+     clave: "C1",
+     duracion: "10 hrs",
+     precio: 800
+    },
+    {
+     nombre: "Introducción a CSS",
+     clave: "C2", 
+     duracion: "12 hrs",
+     precio: 800
+     },
+    {
+     nombre: "CSS avanzado",
+     clave: "C3",
+     duracion: "20 hrs",
+     precio: 1200
+     },
+     {
+      nombre: "Introducción a Javasript",
+      clave: "C4",
+      duracion: "10 hrs",
+      precio: 800
+     }         
+] 
